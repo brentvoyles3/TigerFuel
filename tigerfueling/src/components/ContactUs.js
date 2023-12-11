5960126d551d8df4f9d87a99250fd768530ea025
Brent+ Voyles <brent.voyles3@uga.edu>
2023-12-11 09:14:15 -0500

    Added login logic for creating user w MongoDB

---
+++ tigerfueling/src/components/ContactUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+ 
+const ContactUs = () => {
+    return (
+        <div className="contact">
+            <h1>Contact The Tiger Team</h1>
+            <div>
+            </div>
+        </div>
+    );
+};
+ 
+export default ContactUs;
